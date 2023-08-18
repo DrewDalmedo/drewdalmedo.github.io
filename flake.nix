dd6f0b1f88269f48641fcb69f8ca90c59e07c9cb
Drew Dalmedo <andrewdalmedo@gmail.com>
2023-08-18 06:46:46 -0400

    Create nix flake for dev environment

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Dev environment for working on my personal site.";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }: 
+    let
+      supportedSystems = [ "x86_64-darwin" "x86_64-linux" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs.python311Packages; [
+            mkdocs-material
+            mkdocs-material-extensions
+          ];
+        };
+      });
+    };
+}
