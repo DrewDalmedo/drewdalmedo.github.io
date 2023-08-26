0055dfb0ece55ec018dcf2bfd9e41e0f1215dad5
Drew Dalmedo <andrewdalmedo@gmail.com>
2023-08-26 14:34:25 -0400

    Add flake-utils to build inputs

--- flake.nix
+++ flake.nix
@@ -1,9 +1,12 @@
 {
   description = "Dev environment for working on my personal site.";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs }: 
+  outputs = { self, nixpkgs, flake-utils }: 
     let
       supportedSystems = [ "x86_64-darwin" "x86_64-linux" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
